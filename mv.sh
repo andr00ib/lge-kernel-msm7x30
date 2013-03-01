e697c00cd2587cabbbf46dd4a1ce32a87af4b0fe
andr00ib <andr00ib@gmail.com>
2013-03-01 10:05:44 +0200

    added a script to move zImages and modules

---
+++ mv.sh
@@ -0,0 +1,6 @@
+#Make a folder
+mkdir ~/zImg_wifi
+#Copy module to home folder
+cp drivers/net/wireless/bcm4330/wireless.ko ~/zImg_wifi
+#Copy zImage to home folder
+cp arch/arm/boot/zImage ~/zImg_wifi
